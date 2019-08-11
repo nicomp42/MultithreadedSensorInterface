e8723c4784408b7e2b4626faab5deb7be6ed1766
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-08-11 08:20:05 -0400

    Cleaner

--- MultithreadedSensorInterface/src/main/Main.java
+++ MultithreadedSensorInterface/src/main/Main.java
@@ -10,14 +10,20 @@ import sensor.SensorReader;
 public class Main {
 
 	public static void main(String[] args) {
+		demo();
+	}
+	/**
+	 * This is our app. In the app we read from the sensor and do something, or a lot of somethings. 
+	 */
+	public static void demo() {
 		SensorReader sensorReader = new SensorReader();
 		Sensor temperatureSensor = new Sensor(sensorReader);
 		temperatureSensor.start();	// Start the sensor thread
 		
-		// This is our app. In the app we read from the sensor and do something, or a lot of somethings. 
 		while (true) {
 			try { Thread.sleep(1000);} catch(Exception ex) {}
 			System.out.println("Sensor value = " + sensorReader.getValue());
 		}
+		
 	}
 }
